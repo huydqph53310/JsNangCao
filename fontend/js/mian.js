3b698b2bde62dae34ac19c4d8e602dfd84b4c5e5
huydqph53310 <huydqph53310@gmail.com>
2024-10-14 10:38:49 +0700

    new

--- fontend/js/mian.js
+++ fontend/js/mian.js
@@ -1,12 +1,15 @@
 const storedUser = localStorage.getItem('user'); // Đặt tên biến khác
 const body = document.querySelector('body');
 
-if (storedUser) {
-    const user = JSON.parse(storedUser); // Khai báo một biến mới để chứa dữ liệu đã phân tích
-    console.log(user.user);
-    if (user.role.role !== "admin") {
-        body.innerHTML = "Bạn không có quyền :)";
-    }
-} else {
-    body.innerHTML = "Bạn không có quyền truy cập";
-}
+// if (storedUser) {
+//     const user = JSON.parse(storedUser); // Khai báo một biến mới để chứa dữ liệu đã phân tích
+//     console.log(user.user);
+//     if (user.user.role !== "admin") {
+//         body.innerHTML = "Bạn không có quyền :)";
+//     }
+// } else {
+//     body.innerHTML = "Bạn không có quyền truy cập";
+// }
+
+const card = localStorage.getItem('card')
+console.log(card)
